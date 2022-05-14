cd41cc6c84cc87932126343fb21f97374cc20d5f
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-05-13 20:05:30 -0500

    Add files via upload

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,10 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO Retirement_Titles
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
