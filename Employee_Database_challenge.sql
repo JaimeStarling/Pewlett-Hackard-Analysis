32e42cfaea9f5d18b535d3a618ef01de34eb0477
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-05-15 11:06:58 -0500

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -47,6 +47,7 @@ ORDER BY unique_titles.title
 -- would not order by count column but counts are correct
 SELECT * FROM retiring_titles
 
+-- Deliverable 2
 SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
 	employees.first_name,
 	employees.last_name,
@@ -54,10 +55,58 @@ SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
 	dept_employees.from_date,
 	dept_employees.to_date,
 	titles.title
---INTO mentorship_eligibility
+INTO mentorship_eligibility
 FROM employees
 JOIN dept_employees ON employees.emp_no = dept_employees.emp_no
 JOIN titles on employees.emp_no = titles.emp_no
 WHERE (dept_employees.to_date = '9999-01-01')
 	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility
+
+SELECT * FROM current_emp
+SELECT COUNT FROM (current_emp.emp_no), dept_employees.emp_no
+
+SELECT 
+--DISTINCT ON (unique_titles.title) --unique_titles.title,
+COUNT (mentorship_eligibility.title), mentorship_eligibility.title 
+--INTO retiring_titles
+FROM mentorship_eligibility
+GROUP BY mentorship_eligibility.title;
+-- would not order by count column but counts are correct
+SELECT * FROM mentorship_eligibility
+
+--want retiring by department
+SELECT * FROM ret_by_dept;
+
+--1965 mentors by department
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_employees.dept_no,
+	dept_employees.from_date,
+	dept_employees.to_date,
+	titles.title
+--INTO mship_dept
+FROM employees
+JOIN dept_employees ON employees.emp_no = dept_employees.emp_no
+JOIN titles on employees.emp_no = titles.emp_no
+WHERE (dept_employees.to_date = '9999-01-01')
+	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM emp_info
+
+SELECT emp_info.emp_no,
+	emp_info.gender,
+	emp_info.salary,
+	titles.title,
+	titles.to_date
+INTO salary_compare
+FROM emp_info
+JOIN titles on emp_info.emp_no = titles.emp_no
+WHERE (titles.to_date = '9999-01-01')
+
+SELECT * FROM salary_compare
